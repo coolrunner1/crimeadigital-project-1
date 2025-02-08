ef98e89c6d05c2515ae536b90e83dd3e155cd584
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2025-02-08 16:06:05 +0300

    Added pop-up text to input validation

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -8,6 +8,7 @@ export const InputField = (props: {id: string; label: string; placeholder: strin
                 <input id={props.id} placeholder={"       "+props.placeholder} type="text" required={true}
                        className={"input border border-gray-300 rounded-2xl p-4 [&:not(:placeholder-shown)]:bg-none bg-no-repeat "
                            +(props.invalid ? " invalid:border-red-700 bg-[image:url(/error.svg)]" : props.image)} />
+                {props.invalid && <span className="text-sm text-red-700">Please fill this mandatory field</span>}
             </label>
         </>
     );
