48da20672757a1238a5b4297540e51243cc3a2f0
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2025-02-13 15:29:53 +0300

    Icons are not visible on focus anymore

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -5,7 +5,7 @@ export const InputField = (props: {id: string; label: string; placeholder: strin
         <label htmlFor={props.id} className="flex flex-col text-left text-base font-bold">
             <span>{props.label} <span className="text-red-700">*</span></span>
             <input id={props.id} placeholder={"       "+props.placeholder} type="text" required={true}
-                   className={"input border border-gray-300 rounded-2xl p-4 [&:not(:placeholder-shown)]:bg-none bg-no-repeat "
+                   className={"input border border-gray-300 rounded-2xl p-4 [&:not(:placeholder-shown)]:bg-none focus:bg-none bg-no-repeat "
                        +(props.invalid ? " invalid:border-red-700 bg-[image:url(/error.svg)]" : props.image)} />
             {props.invalid && <span className="text-sm text-red-700">Please fill this mandatory field</span>}
         </label>
