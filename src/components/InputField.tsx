5be7d1f46360b28c48a3d81d734e55cfbf223660
coolrunner1 <waurttttt@gmail.com>
2025-07-09 16:33:57 +0300

    Improved form input

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -12,9 +12,15 @@ export const InputField = (
     return (
         <label htmlFor={props.id} className="flex flex-col text-left text-base font-bold">
             <span>{props.label} <span className="text-red-700">*</span></span>
-            <input id={props.id} placeholder={"       "+props.placeholder} type="text" required={true}
-                   className={"input border border-gray-300 rounded-2xl p-4 [&:not(:placeholder-shown)]:bg-none focus:bg-none bg-no-repeat "
-                       +(props.invalid ? " invalid:border-red-700 bg-[image:url(/error.svg)]" : props.image)} />
+            <div className={`flex flex-row gap-2 border border-gray-300 rounded-2xl p-4`}>
+                <div className={`p-3 bg-no-repeat bg-center ${props.invalid ? ` invalid:border-red-700 bg-[image:url(/error.svg)]` : props.image}`}></div>
+                <input
+                    id={props.id}
+                    placeholder={props.placeholder}
+                    type="text"
+                    required={true}
+                />
+            </div>
             {props.invalid && <span className="text-sm text-red-700">Please fill this mandatory field</span>}
         </label>
     );
