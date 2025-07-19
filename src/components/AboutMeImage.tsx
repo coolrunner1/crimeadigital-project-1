da4136cef87241f255c55a6e46e259005a177d8a
coolrunner1 <waurttttt@gmail.com>
2025-07-19 12:50:32 +0300

    Added lazy loading

--- src/components/AboutMeImage.tsx
+++ src/components/AboutMeImage.tsx
@@ -1,8 +1,18 @@
 export const AboutMeImage = () => {
     return (
         <div className="relative w-auto sm:w-[500px] sm:min-w-[300px] lg:min-w-[450px] overflow-hidden aspect-square m-auto">
-            <img src="/crimeadigital-project-1/man-about.png" alt="Man Main" className="w-[90%] h-[90%] m-[5%] absolute z-10 bg-gray-100 rounded-full aspect-square object-cover" />
-            <img src="/crimeadigital-project-1/man-background.jpeg" alt="man background" className="w-full h-full opacity-70 rounded-full aspect-square"/>
+            <img
+                loading="lazy"
+                src="/crimeadigital-project-1/man-about.png"
+                alt="Man Main"
+                className="w-[90%] h-[90%] m-[5%] absolute z-10 bg-gray-100 rounded-full aspect-square object-cover"
+            />
+            <img
+                loading="lazy"
+                src="/crimeadigital-project-1/man-background.jpeg"
+                alt="man background"
+                className="w-full h-full opacity-70 rounded-full aspect-square"
+            />
         </div>
     );
 };
